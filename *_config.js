6f0feaf53a15cc0b49fa9aa6259a9b99b0c59c0f
mfa <farukalicioglu@gmail.com>
2021-08-09 10:44:49 +0300

    uit

--- *_config.js
+++ *_config.js
@@ -1,8 +1,8 @@
 const config = {};
 
-config.secretVal='Kilis79'
+config.secretVal=''
 
-config.mongodbUrl='mongodb+srv://faruk:32378630@pages.uu1bh.mongodb.net/pages?retryWrites=true&w=majority'
+config.mongodbUrl='mongodb+srv://<username>:<password>@pages.uu1bh.mongodb.net/pages?retryWrites=true&w=majority'
 
 
 module.exports = config;
