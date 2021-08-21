9e7c4a64695194ce5f9c1197263f8e0bb1672a08
Faruk Alıcıoğlu <farukalicioglu@gmail.com>
2021-08-21 20:35:10 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+const http = require('http');
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
-const config = require('./config')
+const config = require('dotenv').config()
 
 const adminRoutes = require('./routes/adminRoutes')
 const pageRoutes = require('./routes/pageRoutes')
 const authRoutes = require('./routes/authRoutes')
 
+
+
 const {requireAuth, checkUser} = require('./middlewares/authMiddleware.js')
 
 const app = express()
 
-const dbUrl = config.mongodbUrl;
+const dbUrl = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_NAME+'.uu1bh.mongodb.net/pages?retryWrites=true&w=majority'
+
+const port = process.env.PORT;
+console.log(port);
 
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true})
 .then((result) => app.listen(3000))
